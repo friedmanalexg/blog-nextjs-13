e7aa068e594fbba980a1b779edd1beae061563e5
Alex <friedmanalexg@gmail.com>
2023-07-28 14:48:55 -0400

    caught tailwind bug

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,9 +3,11 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className='bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10'>
-        <div className='prose prose-xl mx-auto flex justify-between flex-col sm:flex-row'>
-            <Link href="/" className='text-white/90 no-underline hover:text-white'> Alex G Friedman</Link>
+    <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+        <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+            <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+                <Link href="/" className="text-white/90 no-underline hover:text-white"> Alex G Friedman</Link>
+            </h1>
         </div>
     </nav>
   )
