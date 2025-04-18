57bd63e70b306219f10fd53edbd8fe8656f0c495
Alex Friedman <106344806+friedmanalexg@users.noreply.github.com>
2025-04-18 11:36:27 -0400

    add jtc logo finish biz logo element

--- app/components/MyBizLogo.tsx
+++ app/components/MyBizLogo.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 
-// I am currently incomplete. I need to add the logo image to public/images and link it up here.
+
 export default function MyBizLogo() {
   return (
     <section className='w-full mx-auto'>
         <Image 
             className='border-4 border-black dark:border-emerald-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8'
-            src="/images/portrait-600-x-600.jpg"
+            src="/images/jtc600cush.jpg"
             width={200}
             height={200}
             alt='JTC Logo'
